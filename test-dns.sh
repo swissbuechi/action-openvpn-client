2c2d840e9b820b981db27c601afb63373a60ce9e
swissbuechi <raphael.buechi@hotmail.com>
2022-12-24 15:53:34 +0100

    Added scripts to test vpn

--- test-dns.sh
+++ test-dns.sh
@@ -5,7 +5,9 @@ MAX=10
 INTERVAL=1
 COUNTER=0
 while [ $COUNTER -lt $MAX ]; do
+  # shellcheck disable=SC2034
   output=$(dig @"$DNS_SERVER" +short "$DOMAIN_NAME")
+  # shellcheck disable=SC2181
   if [ $? -eq 0 ]; then
     echo "DNS resolution was successful."
     exit 0
--- test-ping.sh
+++ test-ping.sh
@@ -4,7 +4,9 @@ MAX=10
 INTERVAL=1
 COUNTER=0
 while [ $COUNTER -lt $MAX ]; do
+  # shellcheck disable=SC2034
   output=$(ping -c 1 -W 1 "$IP_ADDRESS")
+  # shellcheck disable=SC2181
   if [ $? -eq 0 ]; then
     echo "Ping was successful."
     exit 0
