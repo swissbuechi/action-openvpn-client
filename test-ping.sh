267b358b8df483706772c27daa81ef4584ecca28
swissbuechi <raphael.buechi@hotmail.com>
2022-12-24 17:06:51 +0100

    Refactored Bash Script to inline

--- test-ping.sh
+++ test-ping.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-IP_ADDRESS=$1
-MAX=10
-INTERVAL=1
 COUNTER=0
-while [ $COUNTER -lt $MAX ]; do
+while [ $COUNTER -lt 10 ]; do
   # shellcheck disable=SC2034
-  output=$(ping -c 1 -W 1 "$IP_ADDRESS")
+  output=$(ping -c 1 -W 1 ${{ inputs.test-ping-ip-host }}")
   # shellcheck disable=SC2181
   if [ $? -eq 0 ]; then
     echo "Ping was successful."
     exit 0
   else
-    echo "Ping to $IP_ADDRESS failed."
-    COUNTER=$((COUNTER+INTERVAL))
-    sleep $INTERVAL
+    echo "Ping to ${{ inputs.test-ping-ip-host }} failed."
+    COUNTER=$((COUNTER+1))
+    sleep 1
   fi
 done
 echo "Ping timed out"
